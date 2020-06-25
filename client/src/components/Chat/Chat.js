f8ab86f69c157777874ed39c087f10682a959eb6
Bhavesh Solanki <41828589+bhavesh2699@users.noreply.github.com>
2020-06-26 00:06:03 +0530

    new

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -22,7 +22,7 @@ const Chat = ({ location }) => {
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
-    socket = io(ENDPOINT);
+    socket = io(ENDPOINT,{transports: ['websocket']});
 
     setRoom(room);
     setName(name)
