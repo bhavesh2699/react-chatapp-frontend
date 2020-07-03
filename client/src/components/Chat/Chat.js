2195f57fe927e7b0cb75a5f38d7a438bd6a27fae
Bhavesh Solanki <41828589+bhavesh2699@users.noreply.github.com>
2020-07-03 10:03:28 +0530

    make it better

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -17,7 +17,7 @@ const Chat = ({ location }) => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'ws://chat-backend-app.herokuapp.com/';
+  const ENDPOINT = 'wss://chat-backend-app.herokuapp.com/';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
