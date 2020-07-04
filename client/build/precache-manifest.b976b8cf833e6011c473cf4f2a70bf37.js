6111e21cdbf3a0ae121f7411ca042a51bfd87503
Bhavesh Solanki <41828589+bhavesh2699@users.noreply.github.com>
2020-07-04 10:35:50 +0530

    new

---
+++ client/build/precache-manifest.b976b8cf833e6011c473cf4f2a70bf37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0686b21e6acedd8a4661c565c0b0d33e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d73ecfe2f89944242a45",
+    "url": "/static/css/main.aeac0829.chunk.css"
+  },
+  {
+    "revision": "ba95e79c4a9b72a079dc",
+    "url": "/static/js/2.e942218b.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.e942218b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d73ecfe2f89944242a45",
+    "url": "/static/js/main.6b8d6bd4.chunk.js"
+  },
+  {
+    "revision": "986dd41c12b747f8195a",
+    "url": "/static/js/runtime-main.13167495.js"
+  }
+]);
